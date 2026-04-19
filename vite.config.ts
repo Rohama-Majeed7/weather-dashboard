1b7ee870f70a530e0bb26412665f21317f68bd5b
Rohama Majeed <rohamamajeed3@gmail.com>
2026-04-19 11:15:05 +0300

    pwa feature added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),    tailwindcss(),
-],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true // allows PWA in dev mode (optional but useful)
+      },
+      manifest: {
+        id: '/',
+        name: 'Weather App',
+        short_name: 'App',
+        description: 'My awesome PWA app',
+        theme_color: '#0f172a',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
